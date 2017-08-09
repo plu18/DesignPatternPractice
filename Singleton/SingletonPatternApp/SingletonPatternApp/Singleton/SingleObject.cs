344867ea839af2f5e606295a5d04c335d66e9210
Peiwen Luo <plu18@uclive.ac.nz>
2017-08-09 16:11:17 +1200

    add singleton pattern

---
+++ Singleton/SingletonPatternApp/SingletonPatternApp/Singleton/SingleObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonAPP.Singleton
+{
+    class SingleObject
+    {
+        private static SingleObject instance = new SingleObject();
+
+        //construction function
+        private SingleObject() { }
+
+        public static SingleObject getSingleObjectInstance()
+        {
+
+
+            return instance;
+        }
+
+        public void showMessages()
+        {
+            Console.WriteLine("show singleton message");
+        }
+
+    }
+}
---
+++ Singleton/SingletonPatternApp/SingletonPatternApp/SingletonDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SingletonAPP.Singleton;
+
+namespace SingletonAPP
+{
+    class SingletonDemo
+    {
+        static void Main(string[] args)
+        {
+            SingleObject obj1 = SingleObject.getSingleObjectInstance();
+
+            obj1.showMessages();
+            
+            Console.ReadKey();
+        }
+
+    }
+}
